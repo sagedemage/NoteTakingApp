d51f1275cbe72cc230dcb3149fa700b1c96df17a
Salmaan Saeed <salsaeed999@gmail.com>
2022-07-19 12:58:19 -0400

    set the home and about pages

--- main.go
+++ main.go
@@ -9,13 +9,19 @@ import (
 func main() {
 	router := gin.Default()
 
-	router.LoadHTMLGlob("templates/*")
+	router.LoadHTMLGlob("templates/**/*")
 
 	//router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
 
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl", gin.H {
-			"title": "Main website",
+		c.HTML(http.StatusOK, "home/index.tmpl", gin.H {
+			"title": "Home Page",
+		})
+	})
+
+	router.GET("/about", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "about/index.tmpl", gin.H {
+			"title": "About Page",
 		})
 	})
 	router.Run(":8080") // listen and serve on 0.0.0.0:8080
