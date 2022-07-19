0df28d68b0247fb8b4f1428921a67be10d0c1fac
Salmaan Saeed <salsaeed999@gmail.com>
2022-07-19 12:39:16 -0400

    render my first webpage

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H {
-			"message": "pong",
+	router := gin.Default()
+
+	router.LoadHTMLGlob("templates/*")
+
+	//router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H {
+			"title": "Main website",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	router.Run(":8080") // listen and serve on 0.0.0.0:8080
 }
