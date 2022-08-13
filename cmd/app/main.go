1d8c57f84818e7d5ff78742c49631fa0630a6820
Salmaan Saeed <salsaeed999@gmail.com>
2022-08-13 15:56:42 -0400

    the web app can display a list of products

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -11,6 +12,9 @@ import (
 func main() {
 	// create the router
 	router := gin.Default()
+	
+	// do not trust all proxies for security reasons
+	router.SetTrustedProxies(nil)
 
 	// Load HTML templates
 	router.LoadHTMLGlob("cmd/app/templates/**/*")
@@ -23,6 +27,27 @@ func main() {
 
 	// Migrate the schema
 	db.AutoMigrate(&models.Product{})
+
+	// Product model
+	var product models.Product
+
+	// Create 
+	//db.Create(&models.Product{Name: "Shampoo", Price: 30})
+	
+	/* Read single object from the database */
+	// find the product with integer primary key
+	db.First(&product, 1) 
+	// find product with name Shampoo
+	db.First(&product, "code = ?", "Shampoo") 
+
+	// Update - upgrade product's price to 200
+	db.Model(&product).Update("Price", 200)
+
+	// Update - update multiple fields
+	db.Model(&product).Updates(models.Product{Price: 200, Name: "Shampoo"}) // non-zero fields
+
+	// Delete - delete product
+	//db.Delete(&product, 1)
 
 	// Render the home page at the root of the website
 	router.GET("/", func(c *gin.Context) {
@@ -38,26 +63,22 @@ func main() {
 		})
 	})
 
-	// Create 
-	db.Create(&models.Product{Name: "Shampoo", Price: 20})
+	// Render the table at route "/table"
+	router.GET("/view-table", func(c *gin.Context) {
+		// View all entries of a table
+		var products []models.Product // list
 
-	// Product model
-	var product models.Product
-	
-	/* Read single object from the database */
-	// find the product with integer primary key
-	db.First(&product, 1) 
-	// find product with name Shampoo
-	db.First(&product, "code = ?", "Shampoo") 
+		db.Find(&products)
+		
+		for _, account := range products {
+			fmt.Println(account.Name, account.Price)
+		}
 
-	// Update - upgrade product's price to 200
-	db.Model(&product).Update("Price", 200)
-
-	// Update - update multiple fields
-	db.Model(&product).Updates(models.Product{Price: 200, Name: "Shampoo"}) // non-zero fields
-
-	// Delete - delete product
-	db.Delete(&product, 1)
+		c.HTML(http.StatusOK, "table/index.tmpl", gin.H {
+			"title": "Table",
+			"list": products,
+		})
+	})
 
 	// listen and serve on localhost:8080
 	router.Run(":8080") 
