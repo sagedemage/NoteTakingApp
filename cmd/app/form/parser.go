78099b565e6cf4aa4c344133d3aa43eeb360fbf0
Salmaan Saeed <salsaeed999@gmail.com>
2022-08-21 12:51:22 -0400

    simplified the code to get post form data in the GetFormValue function

--- cmd/app/form/parser.go
+++ cmd/app/form/parser.go
@@ -6,6 +6,6 @@ import (
 
 func GetFormValue(c *gin.Context, named_attribute string) string {
 	/* Get id for a named attribute of the input tag */
-	var value = c.Request.PostFormValue(named_attribute)
+	var value = c.PostForm(named_attribute)
 	return value
 }
