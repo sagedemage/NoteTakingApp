87bc125602eec852d383e6f2b25f42cd61591318
Salmaan Saeed <salsaeed999@gmail.com>
2022-08-27 13:57:39 -0400

    added a unit test to test web pages that require authentication to return 401 error

--- cmd/app/tests/router_test.go
+++ cmd/app/tests/router_test.go
@@ -13,20 +13,74 @@ import (
 	"go-web-app-experiment/cmd/app/router_setup"
 )
 
-func TestRoutes(t *testing.T) {
-  	_, filename, _, _ := runtime.Caller(0)
+func ChangetoRepoRootDirectory() {
+	_, filename, _, _ := runtime.Caller(0)
 
   	dir := path.Join(path.Dir(filename), "../../..")
   	err := os.Chdir(dir)
   	if err != nil {
   	  panic(err)
   	}
+}
 
+func TestRoutes(t *testing.T) {
+	// Change directory
+	ChangetoRepoRootDirectory()
+
+	// setup routerr
 	r := router_setup.InitializeRouter()
-
+	
+	// setup http recorder for testing
 	write := httptest.NewRecorder()
+
+	// test home page
 	request, _ := http.NewRequest("GET", "/", nil)
 	r.ServeHTTP(write, request)
-
 	assert.Equal(t, 200, write.Code)
+
+	// test about page
+	request, _ = http.NewRequest("GET", "/about", nil)
+	r.ServeHTTP(write, request)
+	assert.Equal(t, 200, write.Code)
+
+	// test login page
+	request, _ = http.NewRequest("GET", "/login", nil)
+	r.ServeHTTP(write, request)
+	assert.Equal(t, 200, write.Code)
+
+	// test register page
+	request, _ = http.NewRequest("GET", "/register", nil)
+	r.ServeHTTP(write, request)
+	assert.Equal(t, 200, write.Code)
+}
+
+func TestAuthRoutes(t *testing.T) {
+	// Change directory
+	ChangetoRepoRootDirectory()
+
+	// setup routerr
+	r := router_setup.InitializeRouter()
+	
+	// setup http recorder for testing
+	write := httptest.NewRecorder()
+
+	// test view notes page
+	request, _ := http.NewRequest("GET", "/view-notes", nil)
+	r.ServeHTTP(write, request)
+	assert.Equal(t, 401, write.Code)
+
+	// test about page
+	request, _ = http.NewRequest("GET", "/add-new-note", nil)
+	r.ServeHTTP(write, request)
+	assert.Equal(t, 401, write.Code)
+
+	// test login page
+	request, _ = http.NewRequest("GET", "/edit-note", nil)
+	r.ServeHTTP(write, request)
+	assert.Equal(t, 401, write.Code)
+
+	// test logging out
+	request, _ = http.NewRequest("GET", "/logout", nil)
+	r.ServeHTTP(write, request)
+	assert.Equal(t, 401, write.Code)
 }
