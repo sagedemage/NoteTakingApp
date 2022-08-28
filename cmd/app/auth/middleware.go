5c7c3f8802a034bb9a940a397ad84af53f85ba7c
Salmaan Saeed <salsaeed999@gmail.com>
2022-08-28 13:43:57 -0400

    setup the the web app to render a 401 web page

--- cmd/app/auth/middleware.go
+++ cmd/app/auth/middleware.go
@@ -3,6 +3,8 @@ package auth
 import (
 	//"net/http"
 
+	//"errors"
+
 	"github.com/gin-gonic/gin"
 
 	"go-web-app-experiment/cmd/app/user_session"
@@ -13,13 +15,15 @@ import (
 
 // Middleware to check the user session
 func AuthRequired(c *gin.Context) {
+
+
 	// Get user logged_in session data
 	user := user_session.GetUserSessionData(c, "is_logged_in")
 
 	if user == nil {
-		page_renderer.RenderUnauthorizedWebPage(c, "401.html", "401 Page - Unauthorized")
-		//c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "unathorized"})
-		//return
+		page_renderer.RenderUnauthorizedWebPage(c, "401.html", "Unauthorized")
+
+		return
 	} 
 }
 
--- cmd/app/page_renderer/simple_page.go
+++ cmd/app/page_renderer/simple_page.go
@@ -24,19 +24,3 @@ func RenderWebPage(template_page string, page_title string) gin.HandlerFunc {
 	return gin.HandlerFunc(fn)
 }
 
-func RenderPageNotFoundWebPage(template_page string, page_title string) gin.HandlerFunc {
-	fn := func(c *gin.Context) {
-		/* Render 404 web page */
-		c.HTML(http.StatusNotFound, template_page, gin.H{
-			"page_title": page_title,
-		})
-	}
-	return gin.HandlerFunc(fn)
-}
-
-func RenderUnauthorizedWebPage(c *gin.Context, template_page string, page_title string) {
-	/* Render 401 web page */
-	c.HTML(http.StatusUnauthorized, template_page, gin.H{
-		"page_title": page_title,
-	})
-}
