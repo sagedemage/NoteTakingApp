19fd7b88cf292a182f27a1b7ebbc83b10bb4827f
Salmaan Saeed <salsaeed999@gmail.com>
2022-09-01 22:39:14 -0400

    progress bar increases when there is a lowercase letter and decreases when there is not a lowercase letter for the password in the register page

--- cmd/app/static/js/password_validation.js
+++ cmd/app/static/js/password_validation.js
@@ -1,121 +1,69 @@
 
+
+function ModifyWidthText(current_width) {
+	let width_text = current_width.toString();
+	let new_width_value = width_text.concat("%");
+	
+	return new_width_value
+}
+
 class ProgressBar {
-    /* Class for turning on or off the switch */
-	switch_status;
+	p_bar_width;
 
-	constructor(switch_status) {
-		this.switch_status = switch_status;
-	}
+  	constructor() {
+		this.p_bar_width = 0;
+ 	}
 
-    set_switch(value) {
-        /* set the switch state */
-        this.switch_status = value;
-    }
+	increase_bar(value, cond) {
+		if (cond == false) {
+			this.p_bar_width += value;
+			document.getElementById("p-bar").style.width = ModifyWidthText(this.p_bar_width);
+		
+			console.log(this.p_bar_width);
+			console.log(cond);
 
-    get_switch() {
-        /* get the switch state */
-        return this.switch_status;
-    }
+			cond = true;
+		}
+		return cond;
+  	}
+  
+	decrease_bar(value, cond) {
+		if (cond == true) {
+			this.p_bar_width -= value;
+			document.getElementById("p-bar").style.width = ModifyWidthText(this.p_bar_width);
+			
+			console.log(this.p_bar_width);
+			console.log(cond);
+	
+			cond = false;
+		}
+		return cond;
+  	}
 }
-
-
-let progress_bar = document.getElementById("p-bar");
-let upperCase = new ProgressBar(false);
-
-function ModifyProgressBar(current_width) {
-    /* Increase or Decrease the progress bar */
-    let width_string_new_value = current_width.toString();
-    let new_width_value = width_string_new_value.concat("%");
-    progress_bar.style.width = new_width_value;
-}
-
-function IncreaseProgressBarOnce(switch_object, inc_value, width_current_value) {
-    /* Increase progress bar if the password condition is met */
-    if (switch_object.get_switch() === false) {
-        width_current_value += inc_value;
-        switch_object.set_switch(true);
-    }
-    return width_current_value;
-}
-
-function IncreaseProgressAction() {
-    let p_bar_width_string = progress_bar.style.width;
-    let width_current_value = parseInt(p_bar_width_string.substring(0, p_bar_width_string.length - 1));
-	width_current_value = IncreaseProgressBarOnce(upperCase, 33, width_current_value);
-    ModifyProgressBar(width_current_value);
-}
-
-function DecreaseProgressBarOnce(switch_object, inc_value, width_current_value) {
-    /* Decrease progress bar after removing the character that meet the condition */
-    if (switch_object.get_switch() === true) {
-        width_current_value -= inc_value;
-        switch_object.set_switch(false);
-    }
-    return width_current_value;
-}
-
-
-
-// ---
-
-/*
 
 let passwordField = document.getElementById("password");
-let letter = document.getElementById("letter");
-let lowerCase = new Switch();
-let upperCase = new Switch();
-let num = new Switch();
-let progress_bar = document.getElementById("p-bar");
 
-passwordField.onfocus = function () {
-    / Add effect on the password field when it is on focus /
-    let message = document.getElementById("message");
-    message.style.display = "block";
-    message.style.border = "1px solid";
-}
+let progress_bar = new ProgressBar();
 
-passwordField.onkeyup = function () {
-    // Validate the password on key up //
-    // Validate lowercase letters
-    let lowerCaseLetters = /[a-z]/g;
-    let upperCaseLetters = /[A-Z]/g;
-    let numbers = /[0-9]/g
-    let p_bar_width_string = progress_bar.style.width;
-    let width_current_value = parseInt(p_bar_width_string.substring(0, p_bar_width_string.length - 1));
+let lowerCase = false;
+let upperCase = false;
+let num = false;
 
-    // Increase the progress bar if there is a lowercase letter
+passwordField.onkeyup = function() {
+	/* Validate the password on key up */
+	let lowerCaseLetters = /[a-z]/g;
+	let upperCaseLetters = /[A-Z]/g;
+	let numbers = /[0-9]/g;
+	
     if (passwordField.value.match(lowerCaseLetters)) {
-        width_current_value = IncreaseProgressBarOnce(lowerCase, 33, width_current_value);
-        ModifyProgressBar(width_current_value);
-    }
-    // Decrease the progress bar if there is no lowercase letter
-    else {
-        width_current_value = DecreaseProgressBarOnce(lowerCase, 33, width_current_value);
-        ModifyProgressBar(width_current_value);
+		// Increase the progress bar if there is a lowercase letter
+		lowerCase = progress_bar.increase_bar(33, lowerCase);
     }
 
-    // Increase the progress bar if there is an uppercase letter
-    if (passwordField.value.match(upperCaseLetters)) {
-        width_current_value = IncreaseProgressBarOnce(upperCase, 33, width_current_value);
-        ModifyProgressBar(width_current_value);
-    }
-    // Decrease the progress bar if there is no uppercase letter
-    else {
-        width_current_value = DecreaseProgressBarOnce(upperCase, 33, width_current_value);
-        ModifyProgressBar(width_current_value);
+	else {
+    	// Decrease the progress bar if there is no lowercase letter
+		lowerCase = progress_bar.decrease_bar(33, lowerCase);
     }
 
-    // Increase the progress bar if there is a number
-    if (passwordField.value.match(numbers)) {
-        width_current_value = IncreaseProgressBarOnce(num, 34, width_current_value);
-        ModifyProgressBar(width_current_value);
-    }
-    // Decrease the progress bar if there is no number
-    else {
-        width_current_value = DecreaseProgressBarOnce(num, 34, width_current_value);
-        ModifyProgressBar(width_current_value);
-    }
-} 
-*/
-
+}
 
